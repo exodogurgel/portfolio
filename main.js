ef82f578383e0c7557cedc4eccbd7366bcc1195e
Êxodo <exodowellis@gmail.com>
2022-07-01 17:37:05 -0300

    added backToTopButton

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+function onScroll() {
+  showBackToTopButtonOnScroll()
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
